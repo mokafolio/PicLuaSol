f926208d62270430f6724a8e0635dc62c9c73208
Matthias Dörfelt <hi@mokafolio.de>
2019-11-01 14:52:35 -0700

    fixed problem where sol cant properly push UniquePtr in loadImage

--- PicLuaSol/PicLuaSol.hpp
+++ PicLuaSol/PicLuaSol.hpp
@@ -367,19 +367,27 @@ STICK_API void registerPic(sol::state_view _lua, sol::table _tbl)
     //         return sol::make_object(lua, res.error());
     // });
 
-    _tbl.set_function("loadImage", pic::loadImage);
-    _tbl.set_function("upCastImage", [](Image * _img, sol::this_state _s) {
+    _tbl.set_function("loadImage", [](const char * _path, sol::this_state _s)
+    {
+        sol::state_view lua(_s);
+        auto res = pic::loadImage(_path);
+        if(res)
+            return sol::make_object(lua, std::move(res.get()));
+        return sol::make_object(lua, res.error());
+    });
+    
+    _tbl.set_function("upCastImage", [](pic::ImageUniquePtr & _img, sol::this_state _s) {
         sol::state_view lua(_s);
         //@TODO: complete this
         if (_img->pixelTypeID() == pic::PixelRGB8::pixelTypeID())
         {
-            return sol::make_object(lua, static_cast<pic::ImageRGB8 *>(_img));
+            return sol::make_object(lua, std::move(staticUniquePtrCast<ImageRGB8>(std::move(_img))));
         }
         else if (_img->pixelTypeID() == pic::PixelRGBA8::pixelTypeID())
         {
-            return sol::make_object(lua, static_cast<pic::ImageRGBA8 *>(_img));
+            return sol::make_object(lua, std::move(staticUniquePtrCast<ImageRGBA8>(std::move(_img))));
         }
-        return sol::make_object(lua, _img);
+        return sol::make_object(lua, std::move(_img));
     });
 
     // register concrete image types (we only register 32 bit and floating point image types for
